758012872adb8a944046383ff35c4d1955662ab1
yaneyalvarado <alvyaney@gmail.com>
2023-05-10 19:44:59 -0600

    started code for 'insert into' in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+USE employees
+
+INSERT INTO department(name)
+VALUES ("Software Engineering"), 
+        ("Accounting"),
+        ("Sales"),
+        ("Production"),
+        ("Legal Team"),
+        ("Marketing");
+
+INSERT INTO role (title, salary, department_id)
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
