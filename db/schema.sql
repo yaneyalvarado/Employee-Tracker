6c99b33ee687fb4c942eb3e1cc12f771fe496402
yaneyalvarado <alvyaney@gmail.com>
2023-05-10 19:19:14 -0600

    created role table and employee table

--- db/schema.sql
+++ db/schema.sql
@@ -7,14 +7,19 @@ DROP TABLE IF EXISTS department;
 CREATE TABLE department{
     id: INT PRIMARY KEY
     name: VARCHAR(30) NOT NULL
+};
+
+CREATE TABLE role{
+    id: INT PRIMARY KEY
+    title: VARCHAR(30) NOT NULL
     salary: DECIMAL NOT NULL
     department_id: INT
 };
 
-CREATE TABLE role{
-
-};
-
 CREATE TABLE employee{
-    
+    id: INT PRIMARY KEY
+    first_name: VARCHAR(30) NOT NULL
+    last_name: VARCHAR(30) NOT NULL
+    role_id: INT
+    manager_id: INT
 }
