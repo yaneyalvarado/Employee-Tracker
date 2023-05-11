5c07bd78adf6de5da3dacb514308d46d5b765bf6
yaneyalvarado <alvyaney@gmail.com>
2023-05-10 19:14:58 -0600

    created table for department

--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,8 @@ USE employees;
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department{
-
+    id: INT PRIMARY KEY
+    name: VARCHAR(30) NOT NULL
+    salary: DECIMAL NOT NULL
+    department_id: INT
 };
