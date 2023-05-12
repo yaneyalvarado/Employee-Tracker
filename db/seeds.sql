c1752a7be9ae246bda333c5563e94d71790bca16
yaneyalvarado <alvyaney@gmail.com>
2023-05-12 04:52:43 -0600

    updated code in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,6 +9,13 @@ VALUES ("Software Engineering"),
         ("Marketing");
 
 INSERT INTO role (title, salary, department_id)
-
+    VALUES ("Software Engineer"),
+            ("Accountant"),
+            ("Salesperson"),
+            ("Marketing Manager"),
+            ("Legal Assistant");
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES ("")
+               ("")
+               ("")
