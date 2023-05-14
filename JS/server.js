a5dfdf7d358007b7f5015d6395bbe4d0374fd0c0
yaneyalvarado <alvyaney@gmail.com>
2023-05-14 10:42:06 -0600

    added a function to view all employees in server.js

--- JS/server.js
+++ JS/server.js
@@ -22,5 +22,32 @@ const promptUser = () => {
 }
 .then((answers) => {
     const {choices} = answers;
-    
+    if (choices === 'View all departments') {
+        viewAllDeparmtnets();
+    }
+    if (choices === 'View all roles') {
+        viewAllRoles();
+    }
+    if (choices === 'View all employees') {
+        viewAllEmployees();
+    }
+    if (choices === 'Add a department') {
+        addADepartment();
+    }
+    if (choices === 'Add a role') {
+        addARole();
+    }
+    if (choices === 'Add an employee') {
+        addAEmployee();
+    }
+    if (choices === 'Update an employee role') {
+        updateAnEmployeeRole();
+    }
 })
+
+function viewAllEmployees() {
+    let db = `SELECT employee,id, employee.first_name, employee.last_name, role.title, role.salary, department.name
+     FROM employee, role, department
+     WHERE department.id = role.department_id
+     AND role.id = employee.role_id`
+}
